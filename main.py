d706c870c529c600162f1202442a9ea49c1ceea8
Igrik2009 <132748723+Igrik2009@users.noreply.github.com>
2023-05-13 13:41:16 +0300

    Update 1

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-print('Hello the world!')
-from PyQt5.QtCore import Qt, QTimer, QTime
-from PyQt5.QtWidgets import QApplication, QtWidget, QPushButton, QLable, QVBoxLayout, QHBoxLayout
+# напиши здесь код основного приложения и первого экрана
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text)
+        self.layout.addWidget(self.instruction)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+
+    def next_click(self):
+        self.hide()
+        self.sw = SecondWin()
+        
 app = QApplication([])
-main_win = QtWidget()
-main_win.setWindowTitle('')
-main_win.move(900, 70)
-main_win.resize(400, 200)
-
-
-
-main_win.show()
+mw = MainWin()
 app.exec_()
